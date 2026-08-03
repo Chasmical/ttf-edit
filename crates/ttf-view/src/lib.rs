4367aa3132ad44328c06b8c00500d9d6caee652b
Chasmical <kaz@chsm.dev>
2026-08-04 06:49:56 +0700

    Add OT LongDateTime type

--- crates/ttf-view/src/lib.rs
+++ crates/ttf-view/src/lib.rs
@@ -1,9 +1,10 @@
 #![feature(const_trait_impl)]
 #![feature(const_result_trait_fn)]
 #![feature(const_option_ops)]
-#![feature(const_default)]
 #![feature(const_convert)]
+#![feature(const_default)]
 #![feature(const_clone)]
+#![feature(const_try)]
 #![feature(const_cmp)]
 #![feature(derive_const)]
 #![allow(clippy::missing_safety_doc)] // TODO: remove when adding docs
